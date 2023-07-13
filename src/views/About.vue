d7299f15c897290aef56b5a4e08c850aeb0b9e78
purav-parekh <purav98@gmail.com>
2023-07-13 10:50:09 -0400

    add link to portfolio

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
         <h1>About</h1>
+        <h2>Created by <a href="https://purav-parekh.netlify.app"> Purav Parekh</a></h2>
         <router-link to="/"> Go Back</router-link>
     </div>
 </template>
